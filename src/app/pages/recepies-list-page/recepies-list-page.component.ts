a2e083c7abe6124f0f5f40aa13826d9b4533aac4
Nikonets-Maria <maria.nikonec@gmail.com>
2025-04-02 19:26:01 +0300

    new commit

--- src/app/pages/recepies-list-page/recepies-list-page.component.ts
+++ src/app/pages/recepies-list-page/recepies-list-page.component.ts
@@ -1,17 +1,82 @@
+
 import { Component, inject, OnInit } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { RecepiesService } from '../../service/recepies.service';
+import { RecepItemResponse } from '../../service/recipe/recipe.item.service';
 @Component({
-  selector: 'app-recepies-list-page',
-  imports: [RouterLink, RouterLinkActive],
-  templateUrl: './recepies-list-page.component.html',
-  styleUrl: './recepies-list-page.component.css'
+selector: 'app-recepies-list-page',
+imports: [RouterLink, RouterLinkActive],
+templateUrl: './recepies-list-page.component.html',
+styleUrl: './recepies-list-page.component.css'
 })
+
 export class RecepiesListPageComponent implements OnInit {
-  public recepiServise = inject(RecepiesService)
+public recepiServise = inject(RecepiesService);
+public recepItem: RecepItemResponse[];
+public currentComplexity: string;
+public currentMealType: string;
 
-  ngOnInit(): void {
-    this.recepiServise.getRecipes()
-  }
+constructor(
+private route: ActivatedRoute,
+private router: Router,
+public recepiService: RecepiesService
+) {}
 
+ngOnInit(): void {
+this.recepiServise.getRecipes();
+this.route.params.subscribe(params => {
+this.currentComplexity = params['complexity'];
+this.currentMealType = params['mealType']; // Получаем тип приема пищи из параметров маршрута
+this.recepItem = this.recepiServise.filterRecipesByComplexity(this.currentComplexity);
+this.recepItem = this.recepiServise.filterRecipesByMealType(this.currentMealType); // Применяем фильтрацию по виду приема пищи
+});
 }
+}
+
+
+
+// export class RecepiesListPageComponent implements OnInit {
+//   public recepiServise = inject(RecepiesService)
+//   public recepItem: RecepItemResponse;
+
+//   constructor(
+//     private route: ActivatedRoute,
+//     private router: Router,
+//     // private location: Location,
+//     public recepiService: RecepiesService
+//   ) { }
+//   ngOnInit(): void {
+//     this.recepiServise.getRecipes()
+//   }
+
+  
+
+
+  // public getRecipeByComplexity(id: number | string): Observable<RecepItemResponse> {
+  //   return this.httpClient.get<RecepItemResponse>(this.baseUrl + `/recipes/${id}`);
+  // }
+
+  // public getRecipeByMeal(id: number | string): Observable<RecepItemResponse> {
+  //   return this.httpClient.get<RecepItemResponse>(this.baseUrl + `/recipes/${id}`);
+  // }
+  
+//   ngOnInit(): void {
+//     const recipeId = this.route.snapshot.params['id']
+//     this.recepiService.getRecipeById(recipeId)
+//       .subscribe({
+//         next: (data) => {
+//           // this.recepList = data.recipes; 
+//           this.recepiServise.recepList = data;
+//         },
+//         error: (error) => {
+//           this.router.navigate([''])
+
+//           // Методы для вперед/назад
+//           // this.location.forward()
+//           // this.location.back()
+//         }
+//       })
+
+// }
+
+
--- src/app/service/recepies.service.ts
+++ src/app/service/recepies.service.ts
@@ -8,6 +8,7 @@ import { Observable } from 'rxjs';
 })
 export class RecepiesService {
   private baseUrl = 'https://dummyjson.com'
+  public standartrecepList: RecepItemResponse[] = [] 
   public recepList: RecepItemResponse[] = [] 
 
   constructor(private httpClient: HttpClient) {}
@@ -16,6 +17,7 @@ export class RecepiesService {
     this.httpClient.get<{ recipes: RecepItemResponse[] }>(this.baseUrl + '/recipes')
       .subscribe((data) => {
         this.recepList = data.recipes; 
+        this.standartrecepList = data.recipes; 
         // console.log(this.recepList); 
       });
   }
@@ -24,11 +26,62 @@ export class RecepiesService {
     return this.httpClient.get<RecepItemResponse>(this.baseUrl + `/recipes/${id}`);
   }
 
-  public getRecipeByComplexity(id: number | string): Observable<RecepItemResponse> {
-    return this.httpClient.get<RecepItemResponse>(this.baseUrl + `/recipes/${id}`);
-  }
+  public filterRecipesByComplexity(complexity: string): RecepItemResponse[] {
+    if (complexity === 'all') {
+      // console.log('all')
+      // console.log(this.recepList.length)
+      this.recepList = this.standartrecepList
+      return this.recepList; 
+    } 
+    else if(complexity === 'easy') {
+      this.recepList = this.standartrecepList
 
-  public getRecipeByMeal(id: number | string): Observable<RecepItemResponse> {
-    return this.httpClient.get<RecepItemResponse>(this.baseUrl + `/recipes/${id}`);
+      // console.log('easy')
+      // console.log(this.recepList.filter(recipe => recipe.difficulty.toLowerCase() === 'easy').length)
+      this.recepList = this.recepList.filter(recipe => recipe.difficulty.toLowerCase() === 'easy');
+      return this.recepList
+    } 
+    else {
+      this.recepList = this.standartrecepList
+
+      // console.log('medium')
+      // console.log(this.recepList.filter(recipe => recipe.difficulty.toLowerCase() === 'medium').length)
+
+      this.recepList = this.recepList.filter(recipe => recipe.difficulty.toLowerCase() === 'medium');
+      return this.recepList
+    } 
+}
+
+public filterRecipesByMealType(mealType: string): RecepItemResponse[] {
+  if (mealType === 'breakfast') {
+    this.recepList = this.standartrecepList
+
+    this.recepList = this.recepList.filter(recipe => 
+      recipe.mealType.includes('Breakfast') );
+    return this.recepList;
+  
   }
+  else if (mealType === 'dinner') {
+    this.recepList = this.standartrecepList
+
+    this.recepList = this.recepList.filter(recipe => 
+      recipe.mealType.includes('Dinner') );
+    return this.recepList;
+  
+}
+  else if (mealType === 'lunch') {
+    this.recepList = this.standartrecepList
+
+    this.recepList = this.recepList.filter(recipe => 
+      recipe.mealType.includes('Lunch') );
+    return this.recepList;
+
+}
+else {
+  return this.recepList;}
+  
+}
+
+
+
 }
